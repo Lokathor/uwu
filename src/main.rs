f6d5a88c4455d5284ad54ecc25ac09f903cd4048
Masterchef365 <duncan.freeman1@gmail.com>
2019-12-22 17:19:22 -0800

    More efficient implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::collections::HashMap;
+
+pub struct OWO {
+    map: HashMap<&'static str, &'static str>,
+}
+
+impl OWO {
+    pub fn new() -> Self {
+        OWO {
+            map: [("UwU", "OwO")].iter().cloned().collect(),
+        }
+    }
+
+    pub fn uwu(&self) -> &'static str {
+        return self.map["UwU"].clone();
+    }
+}
+
 fn main() {
-  println!("OwO");
+    let owo = OWO::new();
+    println!("{}", owo.uwu());
 }
